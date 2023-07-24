e35a5a598f57a66c514b6a586e8c0fe8fd1bcb3d
Luigidefilippo <defilippoluigi65@gmail.com>
2023-07-24 10:49:15 +0200

    proprety brinding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,9 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: '<app-server></app-server><app-server></app-server>',
+  // template: '<app-server></app-server><app-server></app-server>',
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
+  allowNesServer = false;
+
+  constructor(){
+    setTimeout(() => {
+      this.allowNesServer = true; 
+    }, 2000); 
+  }
 
 }
